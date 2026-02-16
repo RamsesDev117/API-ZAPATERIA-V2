0177e32899ccfc52cbee513ae3541b321e294396
RamsesDev117 <ramsesdev113@outlook.com>
2026-02-16 17:36:03 -0600

    Se implementaron las relaciones para los modelos

--- src/models/Relaciones.js
+++ src/models/Relaciones.js
@@ -4,6 +4,10 @@ import { Rol } from "./Rol.js";
 import { Empleado } from "./Empleado.js";
 import { Producto } from "./Producto.js";
 import { Inventario } from "./Inventario.js";
+import { Venta } from "./Venta.js";
+import { DetalleVenta } from "./DetalleVenta.js";
+import { Entrada } from "./Entrada.js";
+import { DetalleEntrada } from "./DetalleEntrada.js";
 
 //Definimos nuestras relaciones
 export const defineRelations = () => {
@@ -48,5 +52,56 @@ export const defineRelations = () => {
         as: 'producto'
     })
 
+    // Una Sucursal tiene muchas Ventas
+    Sucursal.hasMany(Venta, {
+        foreignKey: 'sucursal_id', as: 'ventas'
+    });
+    Venta.belongsTo(Sucursal, {
+        foreignKey: 'sucursal_id', as: 'sucursal'
+    });
+
+    // Un Empleado realiza muchas Ventas
+    Empleado.hasMany(Venta, {
+        foreignKey: 'empleado_id', as: 'ventas_realizadas'
+    });
+    Venta.belongsTo(Empleado, {
+        foreignKey: 'empleado_id', as: 'vendedor'
+    });
+
+    // Una Venta tiene muchos Detalles (muchos zapatos)
+    Venta.hasMany(DetalleVenta, {
+        foreignKey: 'venta_id', as: 'detalles'
+    });
+    DetalleVenta.belongsTo(Venta, {
+        foreignKey: 'venta_id', as: 'venta'
+    });
+
+    // Un Detalle de Venta corresponde a un item del Inventario (un SKU-Talla específico)
+    Inventario.hasMany(DetalleVenta, {
+        foreignKey: 'inventario_id', as: 'ventas_detalle'
+    });
+    DetalleVenta.belongsTo(Inventario, {
+        foreignKey: 'inventario_id', as: 'inventario'
+    });
+
+    // Una Entrada (Factura/Lote) tiene muchos detalles (muchos zapatos distintos)
+    Entrada.hasMany(DetalleEntrada, {
+        foreignKey: 'entrada_id',
+        as: 'detalles'
+    });
+    DetalleEntrada.belongsTo(Entrada, {
+        foreignKey: 'entrada_id',
+        as: 'entrada'
+    });
+
+    // Un Producto aparece en muchos detalles de entrada (historial de compras)
+    Producto.hasMany(DetalleEntrada, {
+        foreignKey: 'producto_id',
+        as: 'historial_entradas'
+    });
+    DetalleEntrada.belongsTo(Producto, {
+        foreignKey: 'producto_id',
+        as: 'producto'
+    });
 
 }
